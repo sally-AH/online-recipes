a6d87c86ba196d54519e782248e716a074c312e9
sally-AH <sallyabdalhafeez@gmail.com>
2023-08-21 15:43:27 +0300

    Backend <> Added middlewares

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -7,10 +7,13 @@ use App\Models\User;
 
 class AuthController extends Controller {
 
-    public function __construct() {
-        $this->middleware('auth:api', ['except' => ['login','register']]);
+    public function unauthorized(Request $request) {
+        return response()->json([
+            'status' => 'Error',
+            'message' => 'Unauthorized',
+        ], 401);
     }
-
+    
     public function login(Request $request) {
         $request->validate([
             'email' => 'required|string|email',
@@ -35,7 +38,7 @@ class AuthController extends Controller {
 
     }
 
-    public function register(Request $request){
+    public function register(Request $request) {
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,10 +4,21 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-
+// Unauthenticated APIs
 Route::group(["prefix"=>"guest"], function(){
+    Route::get('unauthorized',[AuthController::class, 'unauthorized'])->name("unauthorized");
     Route::post('login',[AuthController::class, 'login']);
     Route::post('register',[AuthController::class, 'register']);
-    Route::post('logout',[AuthController::class, 'logout']);
-    Route::post('refresh',[AuthController::class, 'refresh']);
+});
+// Authenticated APIs
+Route::group(["middleware"=>"auth:api"], function(){
+
+    Route::group(["middleware"=>"auth.admin"],function(){
+        // admin APIs
+    });
+
+    Route::group(["prefix"=>"user"], function(){
+        Route::post('logout',[AuthController::class, 'logout']);
+        Route::post('refresh',[AuthController::class, 'refresh']);
+    });
 });
